29a743ecb28f601898752cff5170fd412e5251c4
Nick Jonsson <1493113+xjonsson@users.noreply.github.com>
2023-04-14 18:10:47 +0100

    feat(player): added ability to debug player from file

--- src/system/player/Player.ts
+++ src/system/player/Player.ts
@@ -6,30 +6,108 @@ import { NS } from '@ns';
 export class Player {
   id: string;
   data: any;
-  hp: object;
+  hp: { now: boolean; max: boolean };
   programs: any;
   challenge: number;
   bitnode: number;
   city: string;
   location: string;
-  playtime: object;
+  playtime: { total: number; aug: number; node: number };
   money: number;
   level: number;
-  levelRange: object;
-  hack: object;
-  hacknet: object;
-  int: object;
-  str: object;
-  def: object;
-  dex: object;
-  agi: object;
-  cha: object;
-  work: any;
-  faction: object;
-  company: object;
-  crime: object;
-  bladeburner: any;
-  entropy: any;
+  levelRange: { min: number; max: number };
+  hack: {
+    level: number;
+    exp: number;
+    mults: {
+      level: number;
+      exp: number;
+      chance: number;
+      grow: number;
+      speed: number;
+      money: number;
+    };
+  };
+  hacknet: {
+    production: number;
+    node: number;
+    level: number;
+    ram: number;
+    cores: number;
+  };
+  int: {
+    level: number;
+    exp: number;
+  };
+  str: {
+    level: number;
+    exp: number;
+    mults: {
+      level: number;
+      exp: number;
+    };
+  };
+  def: {
+    level: number;
+    exp: number;
+    mults: {
+      level: number;
+      exp: number;
+    };
+  };
+  dex: {
+    level: number;
+    exp: number;
+    mults: {
+      level: number;
+      exp: number;
+    };
+  };
+  agi: {
+    level: number;
+    exp: number;
+    mults: {
+      level: number;
+      exp: number;
+    };
+  };
+  cha: {
+    level: number;
+    exp: number;
+    mults: {
+      level: number;
+      exp: number;
+    };
+  };
+  work: {
+    jobs: any;
+    mults: {
+      money: number;
+    };
+  };
+  faction: {
+    factions: any;
+    mults: {
+      rep: number;
+    };
+  };
+  company: { mults: { rep: number } };
+  crime: {
+    kills: number;
+    mults: {
+      chance: number;
+      money: number;
+    };
+  };
+  bladeburner: {
+    mults: {
+      staminaMax: number;
+      staminaGain: number;
+      analysis: number;
+      chance: number;
+    };
+  };
+  entropy: number;
 
   constructor(ns: NS) {
     this.id = 'player';
@@ -177,4 +255,99 @@ export class Player {
 
     this.entropy = this.data.entropy;
   }
+}
+
+export async function main(ns: NS) {
+  ns.tail();
+  ns.clearLog();
+  ns.disableLog('disableLog');
+  ns.disableLog('sleep');
+  // ns.disableLog('ALL');
+  const p = new Player(ns);
+
+  ns.print(
+    `[Node] ${p.bitnode} [City] ${p.city} [Location] ${p.location} [Entropy] ${p.entropy}`
+  );
+  ns.print(`[HP] ${p.hp.now} / ${p.hp.max}`);
+  ns.print(`[Money] ${ns.formatNumber(p.money, 2)}`);
+  ns.print(`[Level] ${p.level} (${p.levelRange.min} - ${p.levelRange.max})`);
+  ns.print(`[Challenge] ${p.challenge}`);
+  ns.print(`  [Tor] ${p.programs.tor}`);
+  ns.print(`  [SSH] ${p.programs.ssh}`);
+  ns.print(`  [FTP] ${p.programs.ftp}`);
+  ns.print(`  [SMTP] ${p.programs.smtp}`);
+  ns.print(`  [HTTP] ${p.programs.http}`);
+  ns.print(`  [SQL] ${p.programs.sql}`);
+  ns.print(`[Playtime] ${ns.tFormat(p.playtime.total)}`);
+  ns.print(`  [Augment] ${ns.tFormat(p.playtime.aug)}`);
+  ns.print(`  [Bitnode] ${ns.tFormat(p.playtime.node)}`);
+  ns.print(`[Hack]`);
+  ns.print(
+    `  [Mults] Chance: ${p.hack.mults.chance} Grow: ${p.hack.mults.grow} Speed: ${p.hack.mults.speed} Money: ${p.hack.mults.money}`
+  );
+  ns.print(
+    `[Hacknet] Production: ${p.hacknet.production} Node: ${p.hacknet.node} Level: ${p.hacknet.level} Ram: ${p.hacknet.ram} Cores: ${p.hacknet.cores}`
+  );
+  ns.print(`[Stats]`);
+  const header = ' %4s | %5s | %5s | %5s | %6s ';
+  ns.printf(header, 'Stat', 'Level', 'EXP', 'Level', 'EXP');
+  ns.printf(
+    header,
+    'HACK',
+    p.hack.mults.level,
+    p.hack.mults.exp,
+    p.hack.level,
+    ns.formatNumber(p.hack.exp, 2)
+  );
+  ns.printf(header, 'INT', '', '', p.int.level, p.int.exp);
+  ns.printf(
+    header,
+    'STR',
+    p.str.mults.level,
+    p.str.mults.exp,
+    p.str.level,
+    ns.formatNumber(p.str.exp)
+  );
+  ns.printf(
+    header,
+    'DEF',
+    p.def.mults.level,
+    p.def.mults.exp,
+    p.def.level,
+    ns.formatNumber(p.def.exp)
+  );
+  ns.printf(
+    header,
+    'DEX',
+    p.dex.mults.level,
+    p.dex.mults.exp,
+    p.dex.level,
+    ns.formatNumber(p.dex.exp)
+  );
+  ns.printf(
+    header,
+    'AGI',
+    p.agi.mults.level,
+    p.agi.mults.exp,
+    p.agi.level,
+    ns.formatNumber(p.agi.exp)
+  );
+  ns.printf(
+    header,
+    'CHA',
+    p.cha.mults.level,
+    p.cha.mults.exp,
+    p.cha.level,
+    ns.formatNumber(p.cha.exp)
+  );
+  ns.print(`[Work] ${p.work.mults.money}`);
+  ns.print(p.work.jobs);
+  ns.print(`[Faction] ${p.faction.mults.rep}`);
+  ns.print(p.faction.factions);
+  ns.print(`[Company] ${p.company.mults.rep}`);
+  ns.print(
+    `[Crime] Kills ${p.crime.kills} | [Chance] ${p.crime.mults.chance} [Money] ${p.crime.mults.money}`
+  );
+  ns.print(`[Bladeburner]`);
+  ns.print(p.bladeburner);
 }
